66934c6ef25a9216932c7804a61811d714cb1d61
Alex Bennett <alexandergraybennett@gmail.com>
2022-07-28 23:00:04 -0600

    remove node mod

--- index.js
+++ index.js
@@ -6,57 +6,100 @@ try {
   const url = core.getInput("url");
   const token = core.getInput("token");
   const body = core.getInput("body");
+  const websiteurl = core.getInput("websiteurl");
+  const releaseurl = core.getInput("releaseurl");
 
-  axios
-    .post(
-      url,
-      {
-        blocks: [
-          {
-            type: "header",
-            text: {
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
+
+  const commitMessage = github.context.payload?.commits?.[0]?.message;
+  const commitUrl = github.context.payload?.commits?.[0]?.url;
+  const compareUrl = github.context.payload?.compare;
+  const ref = github.context.payload?.ref;
+
+  axios.post(
+    url,
+    {
+      blocks: [
+        {
+          type: "header",
+          text: {
+            type: "plain_text",
+            text: "New Release :rocket:",
+            emoji: true,
+          },
+        },
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: `:computer: <${websiteurl} | Live Site>`,
+          },
+        },
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: `:book: <${releaseurl} | Github Release>`,
+          },
+        },
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: `${body}`,
+          },
+        },
+        {
+          type: "divider",
+        },
+        {
+          type: "section",
+          fields: [
+            {
               type: "plain_text",
-              text: "New Release :rocket:",
+              text: `*Target Branch 🎯* \n ${ref.substring(11)}`,
+              type: "mrkdwn",
+            },
+            {
+              type: "plain_text",
+              text: `*Commit URL ⛓* \n <${commitUrl} | Merge Commit URL>`,
+              type: "mrkdwn",
+            },
+            {
+              type: "plain_text",
+              text: `*Compare 🔍* \n <${compareUrl} | Merge Compare URL>`,
+              type: "mrkdwn",
+            },
+          ],
+        },
+        {
+          type: "divider",
+        },
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: `*Commit Message 📝* \n ${commitMessage}`,
+          },
+        },
+        {
+          type: "divider",
+        },
+        {
+          type: "context",
+          elements: [
+            {
+              type: "plain_text",
+              text: "Made with 💜 Product Science",
               emoji: true,
             },
-          },
-          {
-            type: "section",
-            text: {
-              type: "mrkdwn",
-              text: ":book: <https://google.com|Github Release Code>",
-            },
-          },
-          {
-            type: "section",
-            text: {
-              type: "mrkdwn",
-              text: `${body}`,
-            },
-          },
-          {
-            type: "divider",
-          },
-          {
-            type: "context",
-            elements: [
-              {
-                type: "plain_text",
-                text: "Made with 💜 Product Science",
-                emoji: true,
-              },
-            ],
-          },
-        ],
-      },
-      { headers: { authorization: `Bearer ${token}` } }
-    )
-    .then((response) => {
-      core.setOutput("response", response.data);
-    });
+          ],
+        },
+      ],
+    },
+    { headers: { authorization: `Bearer ${token}` } }
+  );
 
-  // // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
